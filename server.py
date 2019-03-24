5d421fc15614d62da821cf5570f46c4f2cae2ed9
NginxLinode <48883820+NginxLinode@users.noreply.github.com>
2019-03-24 18:18:53 +0200

    v4

---
+++ server.py
@@ -0,0 +1,15 @@
+from bottle import Bottle, run, HTTPResponse
+
+HOST = "0.0.0.0"
+PORT = 80
+BACK_END_SERVER = "tornado"
+HTTP_STATUS_OK = 200
+
+proxy_app = Bottle()
+
+@proxy_app.route("/<url:re:.*>", method=['GET', 'POST'], name='proxy_request_handler')
+def proxy_request_handler(url):
+   return HTTPResponse(status=HTTP_STATUS_OK)
+
+if __name__ == "__main__":
+    run(app=proxy_app, host=HOST, port=int(PORT), server=BACK_END_SERVER)
